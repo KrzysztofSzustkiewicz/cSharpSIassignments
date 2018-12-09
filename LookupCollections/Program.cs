deeeec889be073cbe3c31b1f7cd937df9a891c07
KrzysztofSzustkiewicz <szustkiewicz.k@gmail.com>
2018-12-09 14:53:52 +0100

    ListDictionary excercise

---
+++ LookupCollections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace LookupCollections
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var listDictionary = new ListDictionary(StringComparer.InvariantCultureIgnoreCase)
+            {
+                {"United States", "Estados Unidos"},
+                {"Canada", "Canadá"},
+                {"Spain", "España"}
+            };
+
+            Console.WriteLine(listDictionary["Canada"]);
+            Console.WriteLine(listDictionary["Spain"]);
+        }
+    }
+}
