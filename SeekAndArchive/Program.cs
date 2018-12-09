4bf578c2a846e6e06c38030fc1c7afa32f8d855c
KrzysztofSzustkiewicz <szustkiewicz.k@gmail.com>
2018-12-09 18:38:49 +0100

    finding file directory path

---
+++ SeekAndArchive/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace SeekAndArchive
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Fuck you very much");
+            }
+            else
+            {
+                string directory = args[0];
+                string file = args[1];
+
+                var filesPaths = Directory.GetFiles(directory, file, SearchOption.AllDirectories);
+
+                foreach (var path in filesPaths)
+                {
+                    Console.WriteLine(path);
+                }
+            }
+        }
+    }
+}
