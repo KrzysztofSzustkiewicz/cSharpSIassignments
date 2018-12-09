e405c7eb8f4b533e9d534563f34cbc1c9fc92bf0
KrzysztofSzustkiewicz <szustkiewicz.k@gmail.com>
2018-12-09 12:52:19 +0100

    Queue collection excercise

---
+++ SequentialCollections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace SequentialCollections
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var queue = new Queue();
+            
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Fourth");
+
+            var queueLen = queue.Count;
+
+            for (var i = 0; i < queueLen; i++)
+            {
+                queue.Dequeue();
+            }
+            
+            Console.WriteLine(queue.Count);
+        }
+    }
+}
