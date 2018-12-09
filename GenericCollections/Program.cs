68663eb47e353ba8a3511621ae7503dfc192fa44
KrzysztofSzustkiewicz <szustkiewicz.k@gmail.com>
2018-12-09 15:02:36 +0100

    generic dictionary excercise

---
+++ GenericCollections/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace GenericCollections
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<int, string>()
+            {
+                {93, "Afghanistan"},
+                {355, "Albania"},
+                {213, "Algeria"},
+                {376, "Andorra"},
+                //{"244", "Angola"},
+                //{"672", "Antarctica"}
+            };
+
+            Console.WriteLine(dictionary[93]);
+
+            foreach (var pair in dictionary)
+            {
+                Console.WriteLine(pair.ToString());
+            }
+        }
+    }
+}
