81d603a66395accd70856be2eeddcbe7f68f2199
KrzysztofSzustkiewicz <szustkiewicz.k@gmail.com>
2018-12-09 13:05:45 +0100

    Stack collection excercise

--- SequentialCollections/Program.cs
+++ SequentialCollections/Program.cs
@@ -7,6 +7,8 @@ namespace SequentialCollections
     {
         public static void Main(string[] args)
         {
+            #region QueueCollection
+            
             var queue = new Queue();
             
             queue.Enqueue("First");
@@ -22,6 +24,41 @@ namespace SequentialCollections
             }
             
             Console.WriteLine(queue.Count);
+            
+            #endregion 
+            
+            #region StackCollection
+            
+            var stack = new Stack();
+
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+            
+            PrintStackValues();
+            
+            while (stack.Count != 0)
+            {
+                stack.Pop();
+                PrintStackValues();
+            }
+            
+            Console.WriteLine(stack.Count);
+
+            void PrintStackValues()
+            {
+                foreach (var value in stack)
+                {
+                    Console.WriteLine(value);
+                }
+                Console.WriteLine("\t");
+            }
+
+            #endregion
+            
+            
+
         }
     }
 }
