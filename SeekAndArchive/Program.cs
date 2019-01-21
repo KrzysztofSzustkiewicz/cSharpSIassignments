ea0d1e86a622d8e283d6e548afd7b02a48041b42
KrzysztofSzustkiewicz <szustkiewicz.k@gmail.com>
2019-01-21 10:35:45 +0100

    Jakieś tam zmiany

--- SeekAndArchive/Program.cs
+++ SeekAndArchive/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
 
 namespace SeekAndArchive
 {
@@ -7,6 +9,7 @@ namespace SeekAndArchive
     {
         public static void Main(string[] args)
         {
+            var fileinfo = new FileInfo("Adam.");
             if (args.Length != 2)
             {
                 Console.WriteLine("Fuck you very much");
@@ -22,7 +25,41 @@ namespace SeekAndArchive
                 {
                     Console.WriteLine(path);
                 }
+                
+                
+                #region watcher
+                var watcher = new FileSystemWatcher {Path = directory};
+                
+                watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName |
+                                       NotifyFilters.DirectoryName;
+
+                watcher.Filter = "*.txt";
+                
+                watcher.Changed += OnChanged;
+                watcher.Renamed += OnRenamed;
+
+                watcher.EnableRaisingEvents = true;
+                
+                #endregion
+                
+                Console.WriteLine("Press \'q\' to quit the sample.");
+                while(Console.Read()!='q');
             }
+            
+            
+            
+        }
+        
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " +  e.FullPath + " " + e.ChangeType);
+        }
+        
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
         }
     }
 }
